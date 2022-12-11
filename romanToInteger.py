adfff9b5ca8d8d2ae25cdb6912b85f96d9bf3cf3
RajuKumarAnand <114860221+RajuKumarAnand@users.noreply.github.com>
2022-12-11 06:19:59 +0530

    Add files via upload

---
+++ romanToInteger.py
@@ -0,0 +1,12 @@
+s = input("Roman Number Is:" )
+def romanToInteger(s):
+    d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    num = 0
+    for i in range(len(s)):
+        if i+1 != len(s) and d[s[i]]<d[s[i+1]]:
+            num = num - d[s[i]]
+        else:
+            num = num + d[s[i]]
+    return num
+print("Converted Value InTo Integer Is:",romanToInteger(s))
+
